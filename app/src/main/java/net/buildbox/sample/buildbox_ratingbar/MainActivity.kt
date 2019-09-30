03765091c66e1358f1161823efec60b0ad7904ed
Koji Shigemura <shige0501@gmail.com>
2019-09-30 23:37:57 +0900

    RatingBarの実装

--- app/src/main/java/net/buildbox/sample/buildbox_ratingbar/MainActivity.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_ratingbar/MainActivity.kt
@@ -2,11 +2,18 @@ package net.buildbox.sample.buildbox_ratingbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sampleRating: RatingBar = findViewById(R.id.sample_rating)
+        sampleRating.setOnRatingBarChangeListener { _, rating, _ ->
+            Toast.makeText(this, "現在のレーティングは $rating です", Toast.LENGTH_SHORT).show()
+        }
     }
 }
